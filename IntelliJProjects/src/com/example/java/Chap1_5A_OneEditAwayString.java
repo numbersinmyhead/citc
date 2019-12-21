cb53f6c3b6e6c648554fa5ec1ab5b5c527f55d34
imageset11 <17519046+numbersinmyhead@users.noreply.github.com>
2019-12-21 13:07:30 -0700

    Chap1, Prog5A, Check if the string one edit away
    - Remember how the algorithm works
    - note how to access character at a particular index in a string

--- IntelliJProjects/src/com/example/java/Chap1_5A_OneEditAwayString.java
+++ IntelliJProjects/src/com/example/java/Chap1_5A_OneEditAwayString.java
@@ -15,6 +15,7 @@ public class Chap1_5A_OneEditAwayString {
         String str2 = input.next();
 
         System.out.println("You entered " + str1 + " and " + str2 + ".");
+        System.out.println("Are the two strings replacement edit away? " + isOneEditAway(str1, str2));
     }
 
     // There can possibly be only three kinds of edits: insertion, deletion and replacement.
@@ -36,12 +37,39 @@ public class Chap1_5A_OneEditAwayString {
     }
 
     private static boolean isOneEditTheReplacement (String s1, String s2) {
-        for (char c: s1.toCharArray()) {
-            
+        boolean isOneEditReplacement = false;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                if (isOneEditReplacement) {
+                    return false;
+                }
+                isOneEditReplacement = true;
+            }
         }
+        return true;
     }
 
+    private static boolean isOneEditInsertion(String s1, String s2) {
+        boolean isOneEditInsert = false;
+        int index1 = 0;
+        int index2 = 0;
 
+        while (index1 < s1.length() && index2 < s2.length()) {
+            if (s1.charAt(index1) != s2.charAt(index2)) {
+                if (isOneEditInsert) {
+                    return false;
+                }
+                isOneEditInsert = true;
+                index2++; //index2 refers to string2 i.e. s2. s2 is larger than s1, so we are making up for that offset.
+                //we make up for the offset only one time because we are interested in only one edit.
+            }
+            else {
+                index1++;
+                index2++;
+            }
+        }
+        return true;
+    }
 }
 
 
